90181d4db9e5be274d4546bc2bc3c4356e2624e2
Rafael Ferri <rafael.ferri@nubasolutions.com>
2024-11-04 13:21:25 -0500

    Create vidanta branding for Ricardos demo

--- qonda.config.ts
+++ qonda.config.ts
@@ -1,12 +1,68 @@
-export const company = {
-    company_name: 'qonda',
-    company_display_name: 'Qonda',
-    company_meta_title: 'Qonda | Recruitment made simple',
-    company_favicon_url: '/assets/companies/qonda/favicon.ico',
-    company_logo_url: '/assets/companies/qonda/qonda-logo.svg',
-    company_logo_white_url: '/assets/companies/qonda/qonda-logo-white.svg',
-    company_color_schema_class: '',
-    company_login_bg: '/assets/companies/qonda/qonda-login.png',
+// export const company = {
+//     company_name: 'qonda',
+//     company_display_name: 'Qonda',
+//     company_meta_title: 'Qonda | Recruitment made simple',
+//     company_favicon_url: '/assets/companies/qonda/favicon.ico',
+//     company_logo_url: '/assets/companies/qonda/qonda-logo.svg',
+//     company_logo_white_url: '/assets/companies/qonda/qonda-logo-white.svg',
+//     company_color_schema_class: '',
+//     company_login_bg: '/assets/companies/qonda/qonda-login.png',
+//     company_settings: {
+//         candidates_rules: {
+//             description:
+//                 'Each status belongs to a specific step in the HR funnel. These are the applicable rules per status indicating in which step a candidate will end up once their status is changed. For further information contact your system administrator.',
+//             statuses: {
+//                 applicants: [
+//                     {
+//                         id: 1,
+//                         name: 'interview',
+//                         en: 'Interview',
+//                         es: 'Entrevista',
+//                     },
+//                     {
+//                         id: 2,
+//                         name: 'in_process',
+//                         en: 'In process',
+//                         es: 'En processo',
+//                     },
+//                 ],
+//                 employees: [
+//                     {
+//                         id: 3,
+//                         name: 'hired',
+//                         en: 'Hired',
+//                         es: 'Contratado',
+//                     },
+//                     {
+//                         id: 4,
+//                         name: 'promoted',
+//                         en: 'Promoted',
+//                         es: 'Promovido',
+//                     },
+//                 ],
+//                 former_employees: [
+//                     {
+//                         id: 5,
+//                         name: 'fired',
+//                         en: 'Fired',
+//                         es: 'Desligado',
+//                     },
+//                 ],
+//             },
+//         },
+//     },
+// }
+
+export const companyW = {
+    company_name: 'wingstop',
+    company_display_name: 'Wingstop',
+    company_meta_title: 'Wingstop | HR',
+    company_favicon_url: '/assets/companies/wingstop/favicon.ico',
+    company_logo_url: '/assets/companies/wingstop/wingstop-logo.svg',
+    company_logo_white_url:
+        '/assets/companies/wingstop/wingstop-logo-white.svg',
+    company_color_schema_class: 'wingstop',
+    company_login_bg: '/assets/companies/wingstop/wingstop-login.png',
     company_settings: {
         candidates_rules: {
             description:
@@ -15,34 +71,100 @@ export const company = {
                 applicants: [
                     {
                         id: 1,
-                        name: 'interview',
-                        en: 'Interview',
-                        es: 'Entrevista',
+                        name: 'personnel_requisition',
+                        en: 'Personnel Requisition',
+                        es: 'Requisición de Personal',
                     },
                     {
                         id: 2,
-                        name: 'in_process',
-                        en: 'In process',
-                        es: 'En processo',
+                        name: 'kick_off',
+                        en: 'Kick Off',
+                        es: 'Kick Off',
+                    },
+                    {
+                        id: 3,
+                        name: 'job_posting',
+                        en: 'Job Posting',
+                        es: 'Publicación de Puesto',
+                    },
+                    {
+                        id: 4,
+                        name: 'referral_program',
+                        en: 'Referral Program',
+                        es: 'Programa de Referencia',
+                    },
+                    {
+                        id: 5,
+                        name: 'universal_registration_of_candidates',
+                        en: 'Universal Registration of Candidates',
+                        es: 'Registro Universal de candidatos',
+                    },
+                    {
+                        id: 6,
+                        name: 'pre_screening',
+                        en: 'Pre Screening',
+                        es: 'Pre Screening',
+                    },
+                    {
+                        id: 7,
+                        name: 'interview_selection',
+                        en: 'Interview Selection',
+                        es: 'Entrevista Selección',
                     },
                 ],
                 employees: [
                     {
-                        id: 3,
-                        name: 'hired',
-                        en: 'Hired',
-                        es: 'Contratado',
+                        id: 8,
+                        name: 'evaluations',
+                        en: 'Evaluations',
+                        es: 'Evaluaciones',
                     },
                     {
-                        id: 4,
-                        name: 'promoted',
-                        en: 'Promoted',
-                        es: 'Promovido',
+                        id: 9,
+                        name: 'manager_interview',
+                        en: 'Manager Interview',
+                        es: 'Entrevista Jefe de Área',
+                    },
+                    {
+                        id: 10,
+                        name: 'medical_check',
+                        en: 'Medical Check',
+                        es: 'Medical Check',
+                    },
+                    {
+                        id: 11,
+                        name: 'background_check',
+                        en: 'Background Check',
+                        es: 'Background Check',
+                    },
+                    {
+                        id: 12,
+                        name: 'job_offer',
+                        en: 'Job Offer',
+                        es: 'Job Offer',
+                    },
+                    {
+                        id: 13,
+                        name: 'documents',
+                        en: 'Documents',
+                        es: 'Documentos',
+                    },
+                    {
+                        id: 14,
+                        name: 'induction',
+                        en: 'Induction',
+                        es: 'Inducción',
+                    },
+                    {
+                        id: 15,
+                        name: 'hired',
+                        en: 'Hired',
+                        es: 'Contratación',
                     },
                 ],
                 former_employees: [
                     {
-                        id: 5,
+                        id: 20,
                         name: 'fired',
                         en: 'Fired',
                         es: 'Desligado',
@@ -53,124 +175,123 @@ export const company = {
     },
 }
 
-// export const company = {
-//     company_name: 'wingstop',
-//     company_display_name: 'Wingstop',
-//     company_meta_title: 'Wingstop | HR',
-//     company_favicon_url: '/assets/companies/wingstop/favicon.ico',
-//     company_logo_url: '/assets/companies/wingstop/wingstop-logo.svg',
-//     company_logo_white_url:
-//         '/assets/companies/wingstop/wingstop-logo-white.svg',
-//     company_color_schema_class: 'wingstop',
-//     company_login_bg: '/assets/companies/wingstop/wingstop-login.png',
-//     company_settings: {
-//         candidates_rules: {
-//             description:
-//                 'Each status belongs to a specific step in the HR funnel. These are the applicable rules per status indicating in which step a candidate will end up once their status is changed. For further information contact your system administrator.',
-//             statuses: {
-//                 applicants: [
-//                     {
-//                         id: 1,
-//                         name: 'personnel_requisition',
-//                         en: 'Personnel Requisition',
-//                         es: 'Requisición de Personal',
-//                     },
-//                     {
-//                         id: 2,
-//                         name: 'kick_off',
-//                         en: 'Kick Off',
-//                         es: 'Kick Off',
-//                     },
-//                     {
-//                         id: 3,
-//                         name: 'job_posting',
-//                         en: 'Job Posting',
-//                         es: 'Publicación de Puesto',
-//                     },
-//                     {
-//                         id: 4,
-//                         name: 'referral_program',
-//                         en: 'Referral Program',
-//                         es: 'Programa de Referencia',
-//                     },
-//                     {
-//                         id: 5,
-//                         name: 'universal_registration_of_candidates',
-//                         en: 'Universal Registration of Candidates',
-//                         es: 'Registro Universal de candidatos',
-//                     },
-//                     {
-//                         id: 6,
-//                         name: 'pre_screening',
-//                         en: 'Pre Screening',
-//                         es: 'Pre Screening',
-//                     },
-//                     {
-//                         id: 7,
-//                         name: 'interview_selection',
-//                         en: 'Interview Selection',
-//                         es: 'Entrevista Selección',
-//                     },
-//                 ],
-//                 employees: [
-//                     {
-//                         id: 8,
-//                         name: 'evaluations',
-//                         en: 'Evaluations',
-//                         es: 'Evaluaciones',
-//                     },
-//                     {
-//                         id: 9,
-//                         name: 'manager_interview',
-//                         en: 'Manager Interview',
-//                         es: 'Entrevista Jefe de Área',
-//                     },
-//                     {
-//                         id: 10,
-//                         name: 'medical_check',
-//                         en: 'Medical Check',
-//                         es: 'Medical Check',
-//                     },
-//                     {
-//                         id: 11,
-//                         name: 'background_check',
-//                         en: 'Background Check',
-//                         es: 'Background Check',
-//                     },
-//                     {
-//                         id: 12,
-//                         name: 'job_offer',
-//                         en: 'Job Offer',
-//                         es: 'Job Offer',
-//                     },
-//                     {
-//                         id: 13,
-//                         name: 'documents',
-//                         en: 'Documents',
-//                         es: 'Documentos',
-//                     },
-//                     {
-//                         id: 14,
-//                         name: 'induction',
-//                         en: 'Induction',
-//                         es: 'Inducción',
-//                     },
-//                     {
-//                         id: 15,
-//                         name: 'hired',
-//                         en: 'Hired',
-//                         es: 'Contratación',
-//                     },
-//                 ],
-//                 former_employees: [
-//                     {
-//                         id: 20,
-//                         name: 'fired',
-//                         en: 'Fired',
-//                         es: 'Desligado',
-//                     },
-//                 ],
-//             },
-//         },
-//     },
-// }
+export const company = {
+    company_name: 'vidanta',
+    company_display_name: 'Vidanta',
+    company_meta_title: 'Vidanta | HR',
+    company_favicon_url: '/assets/companies/vidanta/favicon.ico',
+    company_logo_url: '/assets/companies/vidanta/vidanta-logo.svg',
+    company_logo_white_url: '/assets/companies/vidanta/vidanta-logo-white.svg',
+    company_color_schema_class: 'vidanta',
+    company_login_bg: '/assets/companies/vidanta/vidanta-login-bg.png',
+    company_settings: {
+        candidates_rules: {
+            description:
+                'Each status belongs to a specific step in the HR funnel. These are the applicable rules per status indicating in which step a candidate will end up once their status is changed. For further information contact your system administrator.',
+            statuses: {
+                applicants: [
+                    {
+                        id: 1,
+                        name: 'personnel_requisition',
+                        en: 'Personnel Requisition',
+                        es: 'Requisición de Personal',
+                    },
+                    {
+                        id: 2,
+                        name: 'kick_off',
+                        en: 'Kick Off',
+                        es: 'Kick Off',
+                    },
+                    {
+                        id: 3,
+                        name: 'job_posting',
+                        en: 'Job Posting',
+                        es: 'Publicación de Puesto',
+                    },
+                    {
+                        id: 4,
+                        name: 'referral_program',
+                        en: 'Referral Program',
+                        es: 'Programa de Referencia',
+                    },
+                    {
+                        id: 5,
+                        name: 'universal_registration_of_candidates',
+                        en: 'Universal Registration of Candidates',
+                        es: 'Registro Universal de candidatos',
+                    },
+                    {
+                        id: 6,
+                        name: 'pre_screening',
+                        en: 'Pre Screening',
+                        es: 'Pre Screening',
+                    },
+                    {
+                        id: 7,
+                        name: 'interview_selection',
+                        en: 'Interview Selection',
+                        es: 'Entrevista Selección',
+                    },
+                ],
+                employees: [
+                    {
+                        id: 8,
+                        name: 'evaluations',
+                        en: 'Evaluations',
+                        es: 'Evaluaciones',
+                    },
+                    {
+                        id: 9,
+                        name: 'manager_interview',
+                        en: 'Manager Interview',
+                        es: 'Entrevista Jefe de Área',
+                    },
+                    {
+                        id: 10,
+                        name: 'medical_check',
+                        en: 'Medical Check',
+                        es: 'Medical Check',
+                    },
+                    {
+                        id: 11,
+                        name: 'background_check',
+                        en: 'Background Check',
+                        es: 'Background Check',
+                    },
+                    {
+                        id: 12,
+                        name: 'job_offer',
+                        en: 'Job Offer',
+                        es: 'Job Offer',
+                    },
+                    {
+                        id: 13,
+                        name: 'documents',
+                        en: 'Documents',
+                        es: 'Documentos',
+                    },
+                    {
+                        id: 14,
+                        name: 'induction',
+                        en: 'Induction',
+                        es: 'Inducción',
+                    },
+                    {
+                        id: 15,
+                        name: 'hired',
+                        en: 'Hired',
+                        es: 'Contratación',
+                    },
+                ],
+                former_employees: [
+                    {
+                        id: 20,
+                        name: 'fired',
+                        en: 'Fired',
+                        es: 'Desligado',
+                    },
+                ],
+            },
+        },
+    },
+}
